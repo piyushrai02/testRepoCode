34852f78ead250c00897e031a9357e39ed995e37
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2025-12-29 11:30:35 +0530

    Clean up whitespace in server.js
    Removed unnecessary whitespace in server.js.

--- blog-api/server.js
+++ blog-api/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = 5000;
 
 const SECRET_KEY = 'supersecretkey'; // In a real app, use environment variables
-const SECRET_KEY1 = 'supersecretkey1'; // In a real app, use environment variables
+const SECRET_KEY1 = 'supersecretkey1';
 
 
 app.use(bodyParser.json());
