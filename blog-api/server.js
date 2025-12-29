eea46745cfa55a41fa45dbb18290ed039c602adc
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2025-12-29 22:53:15 +0530

    Fix variable name for secret key in server.js

--- blog-api/server.js
+++ blog-api/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = 5000;
 
 const SECRET_KEY = 'supersecretkey'; // In a real app, use environment variables
-const SECRET_KEY47 = 'supersecretkey'; // In a real app, use environment variables
+const SECRET_KEY4 = 'supersecretkey'; // In a real app, use environment variables
 
 app.use(bodyParser.json());
 console.log("Same add the log for tesing")
