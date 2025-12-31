1f1ec2918bb64a610883d618c19d4217e635b737
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2025-12-31 23:34:04 +0530

    Update server.js

--- blog-api/server.js
+++ blog-api/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = 5000;
 
 const SECRET_KEY = 'supersecretkey'; // In a real app, use environment variables
-const SECRET_KEY5qw = 'supersecretkey'; // In a real app, use environment variables
+const SECRET_KEY5w = 'supersecretkey'; // In a real app, use environment variables
 
 app.use(bodyParser.json());
 console.log("Same add the log for tesing")
