bf48e898d41d41a0199cd5604f5901d6fedb608f
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-01-16 10:42:34 +0530

    Update server.js

--- blog-api/server.js
+++ blog-api/server.js
@@ -11,7 +11,7 @@ const SECRET_KEY = 'supersecretkey'; // In a real app, use environment variables
 const SECRET_KEYw = 'supersecretkey'; // In a real app, use environment variables
 
 app.use(bodyParser.json());
-console.log("Same add the log for tesing dsaddwq weeweq")
+console.log("Same add the log for tesing dsaddwq weeweqvqwddqwdwq")
 // loaded for the same 
 app.use(cors());
 let users = []; // In-memory user storage for simplicity
