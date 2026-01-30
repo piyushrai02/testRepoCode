6eb69a47208bbaf6f737d615af1ececca314174d
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-01-30 23:28:08 +0530

    Fix placeholder text in SearchBar component

--- todo-app/src/components/SearchBar.js
+++ todo-app/src/components/SearchBar.js
@@ -5,7 +5,7 @@ const SearchBar =  => {
     <div className="search-bar">
       <input
         type="text"
-        placeholder="Search...."
+        placeholder="Search..."
         value={value}
         onChange={onChange}
         className="todo-input search-input"
