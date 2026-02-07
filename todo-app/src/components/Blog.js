bb2d43564d9975717feff07597457e5303ade185
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-02-07 22:10:13 +0530

    Update Blog.js

--- todo-app/src/components/Blog.js
+++ todo-app/src/components/Blog.js
@@ -6,7 +6,7 @@ const Blog = ({ token }) => {
   const [posts, setPosts] = useState([0]);
 
   useEffect(() => {
-    const storedPosts = JSON.parse(localStorage.getItem('posts1'));
+    const storedPosts = JSON.parse(localStorage.getItem('posts'));
     if (storedPosts) {
       setPosts(storedPosts);
     }
