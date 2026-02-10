060e6191cb6f6034fb2b7d485258891b21a8a955
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-02-10 16:23:32 +0530

    Update SearchBar.js

--- todo-app/src/components/SearchBar.js
+++ todo-app/src/components/SearchBar.js
@@ -5,7 +5,7 @@ const SearchBar =  => {
     <div className="search-bar">
       <input
         type="text"
-        placeholder="Search for themsa...w"
+        placeholder="Search for themsa..."
         value={value}
         onChange={onChange}
         className="todo-input search-input"
