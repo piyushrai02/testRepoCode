e9efe89169d969e1bfa358a55304b26d8940694c
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-02-14 11:33:20 +0530

    Update todo-app/src/components/Todo.js
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- todo-app/src/components/Todo.js
+++ todo-app/src/components/Todo.js
@@ -21,7 +21,7 @@ const Todo = () => {
 
   // Load todos from localStorage on mount
   useEffect(() => {
-    const savedTodos1 = localStorage.getItem('todos');
+    const savedTodos = localStorage.getItem('todos');
     if (savedTodos) {
       setTodos(JSON.parse(savedTodos));
     }
