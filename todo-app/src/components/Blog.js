4f6c46b58e6dd2077206c2a46db23eec9585bf5a
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-02-16 11:34:20 +0530

    Update Blog.js

--- todo-app/src/components/Blog.js
+++ todo-app/src/components/Blog.js
@@ -5,7 +5,7 @@ import BlogList from './BlogList';
 const Blog = ({ token }) => {
   const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
+  useEffect(() ={
     const storedPosts = JSON.parse(localStorage.getItem('posts1'));
     if (storedPosts1) {
       setPosts(storedPosts);
