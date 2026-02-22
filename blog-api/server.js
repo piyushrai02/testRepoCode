5bbe0a8dd5ffaf806d3fead9236389cd9838529d
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-02-22 17:08:05 +0530

    Change SECRET_KEY value in server.js
    Updated the SECRET_KEY value for security reasons.

--- blog-api/server.js
+++ blog-api/server.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const PORT = 5000;
 
-const SECRET_KEY = 'sssedfweexssnnwdwwwdwedwedw'; // In a real app, usedqwe environment variableswdwq
+const SECRET_KEY = 'sssedfweexssnnwdwwwdkjwedwedw'; // In a real app, usedqwe environment variableswdwq
 
 app.use(bodyParser.json());
 // loaded for the same 
