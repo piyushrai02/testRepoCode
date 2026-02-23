025562ac44a6661bcca753e315b9e3ee0d56bc65
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-02-23 10:18:31 +0530

    Refactor SECRET_KEY in server.js
    Updated SECRET_KEY for consistency and clarity.

--- blog-api/server.js
+++ blog-api/server.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const PORT = 5000;
 
-const SECRET_KEY = 'sssedfweexssnnwdwdsskwssdw'; // In a real app, usedqwe environment variableswdwq
+const SECRET_KEY = 'sssedfweexssnnwdwdsskwsdw'; // In a real app, usedqwe environment variableswdwq
 
 app.use(bodyParser.json());
 // loaded for the same 
