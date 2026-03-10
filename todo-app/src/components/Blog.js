a9975ac657bc8d80296dd8a327ab2d462893e790
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-03-10 16:27:35 +0530

    Update Blog.js

--- todo-app/src/components/Blog.js
+++ todo-app/src/components/Blog.js
@@ -14,7 +14,7 @@ useEffect(() => {
     } catch (e) {
       console.error('Failed to parsse posts from losscalStorage:', e);
       // Optionally clear the corrupted data
-      localStorage.removeItem('posts1');
+      localStorage.removeItem('posts');
     }
     if (storedPosts) {
       setPosts(storedPosts);
