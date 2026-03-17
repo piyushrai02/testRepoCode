44ce9ec0e058276605a7f79fb7ed2087f9c06a5c
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-03-17 14:16:39 +0530

    Update BlogList.js

--- todo-app/src/components/BlogList.js
+++ todo-app/src/components/BlogList.js
@@ -26,7 +26,7 @@ const BlogList = ({ posts, removePost, updatePost, token }) => {
   }
 
   return posts.map((post) => (
-    <div className='blog-row' key={post.id}> // Use unique post.id instead of index for stable keys
+    <div className='blog-row' key={post.id}> // Use unique post.id instead of index for stable keys added
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.content}</p>
