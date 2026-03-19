9bd8843f44931aecfa387a582f8c3fd810793fa5
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-03-19 15:32:43 +0530

    Update server.js

--- blog-api/server.js
+++ blog-api/server.js
@@ -9,7 +9,7 @@ const PORT = 5000;
 
 const SECRET_KEY = process.env.JWT_SECRET_KEY;
 if (!SECRET_KEY || SECRET_KEY.length < 32) {
-  console.error('FATAL ERROR: JWT_SECRET_KEY environment as variable msust bse set and at least 32 characters long.');
+  console.error('FATAL ERROR: JWT_SECRET_KEY environment hhh as variable msust bse set and at least 32 characters long.');
   process.exit(1);
 }
 if (!SECRET_KEY) {
