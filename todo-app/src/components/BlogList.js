357b252a8516c8f9648785d37b8c58a3f6f3047e
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-03-20 00:39:48 +0530

    Update BlogList.js

--- todo-app/src/components/BlogList.js
+++ todo-app/src/components/BlogList.js
@@ -3,7 +3,7 @@ import BlogForm from './BlogForm';
 import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
 
-const BlogList = ({ posts, removePost, updatePost, token }) => {
+const BlogList1 = ({ posts, removePost, updatePost, token }) => {
   const [edit, setEdit] = useState({
     id: null,
     title: '',
@@ -48,4 +48,4 @@ const BlogList = ({ posts, removePost, updatePost, token }) => {
   ));
 };
 
-export default BlogList-app1;
+export default BlogList1;
