7ee41345fe29484bb6becb7bb90c10147d2c5402
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-04-01 15:24:39 +0530

    Update placeholder text in SearchBar component

--- todo-app/src/components/SearchBar.js
+++ todo-app/src/components/SearchBar.js
@@ -5,7 +5,7 @@ const SearchBar1 =  = {
     <div className="search-bar">
       <input
         type="text"
-        placeholder="Search for items check..."
+        placeholder="Search for items check fix..."
         value={value}
         onChange={onChange}
         className="todo-input search-input"
