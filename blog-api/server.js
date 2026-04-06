8b58c79bf0aa399faf89a88732084694e791bf15
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-04-06 12:45:47 +0530

    Update server.js

--- blog-api/server.js
+++ blog-api/server.js
@@ -9,7 +9,7 @@ const PORT = 5000;
 
 const SECRET_KEY = process.env.JWT_SECRET_KEY;
 if (!SECRET_KEY || SECRET_KEY.length < 32) {
-  console.error('FATAL ERROR: JWT_dSECRET_KEY  endvironmend variadble  msust bse set and at  chekc fix 3least 32 character long  Check.');
+  console.error('FATAL ERROR: JWT_dSECRET_KEY  endvironmendd variadble  msust bse set and at  chekc fix 3least 32 character long  Check.');
   process.exit(1);
 }
 if (!SECRET_KEY) {
