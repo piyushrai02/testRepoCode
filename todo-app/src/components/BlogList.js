595d7939c864f0f1887c1bfb9a2bd5018f8dbeae
Piyush Rai <56262183+piyushrai02@users.noreply.github.com>
2026-04-26 20:02:40 +0530

    Rename BlogList2 component to BlogList4

--- todo-app/src/components/BlogList.js
+++ todo-app/src/components/BlogList.js
@@ -3,7 +3,7 @@ import BlogForm from './BlogForm';
 import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
 
-const BlogList2 = ({ posts, removePost, updatePost, token }) => {
+const BlogList4 = ({ posts, removePost, updatePost, token }) => {
   const [edit, setEdit] = useState({
     id: null,
     title: '',
